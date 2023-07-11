d6761b71740514227610b774fc974f33dc31b56d
Amine derbal <aminedoran31@gmail.com>
2023-07-11 11:48:11 +0100

    Add routes to the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Greeting from './components/Greeting';
+import './App.css';
 
-const App = () => <Greeting />;
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Greeting />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
